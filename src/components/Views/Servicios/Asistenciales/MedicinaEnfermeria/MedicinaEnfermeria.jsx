94fbc459d785f42d88b6e2f4a2c84ca3c7df0a10
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-04-12 12:35:52 +0200

    Add more routes in services

---
+++ src/components/Views/Servicios/Asistenciales/MedicinaEnfermeria/MedicinaEnfermeria.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MedicinaEnfermeria.css'
+
+const MedicinaEnfermeria = () => {
+    return (
+        <div>
+            <h1>Medicina y enfermeria</h1>
+        </div>
+    );
+};
+
+export default MedicinaEnfermeria;
---
+++ src/components/Views/Servicios/Terapias/Psicologia/Psicologia.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Psicologia.css'
+
+const Psicologia = () => {
+    return (
+        <div>
+            <h1>Psicología</h1>
+        </div>
+    );
+};
+
+export default Psicologia;
