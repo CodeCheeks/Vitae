cdb9c66c276ace8c7ececfa330f36d307ebe0ed1
Alicia Parra <alicia@redparra.com>
2021-04-15 17:11:17 +0200

    Change footer

--- src/components/CustomFooter/CustomFooter.jsx
+++ src/components/CustomFooter/CustomFooter.jsx
@@ -10,16 +10,19 @@ import './CustomFooter.css'
 
     return (
         <div className="container-fluid footer__container">
-            <div className="row mb-2">
+            <div className="row mb-2 ">
                 <div className="col border-right">
-                    <h5 className="text-light">Servicios</h5>
-                    <ul className="list-group list-group-flush">
-                        <li className=""><Link to="/servicios/terapias" className="text-light">» Terapéuticos</Link></li>
-                        <li className=""><Link to="/servicios/asistenciales" className="text-light">» Asistenciales</Link></li>
-                    </ul>
-                    <h5 className="text-light mt-3">Contacto</h5>
-                    <h6 className="text-light">exam@vita.com</h6>
-                    <h6 className="text-light">📞 9121765942</h6>
+                    <div className='mb-5 d-flex flex-column justify-content-between'>
+                        <h5 className="text-light mt-3">Contacto</h5>
+                        <h6 className="text-light footer__text__1 my-2">exam@vita.com</h6>
+                        <h6 className="text-light footer__text__1 my-2">📞 9121765942</h6>
+                    </div>
+                    <div className='d-flex justify-content-around flex-wrap'>
+                        <a href='https://www.facebook.com/' target="_blank" rel="noreferrer"><img src='https://res.cloudinary.com/dv4pkbmin/image/upload/v1618498664/Icon/facebook_ljni9y.png' alt='Facebook' width='50' height='50'/></a>
+                        <a href='https://www.facebook.com/' target="_blank" rel="noreferrer"><img src='https://res.cloudinary.com/dv4pkbmin/image/upload/v1618498683/Icon/gorjeo_ntdnvn.png' alt='Twitter' width='50' height='50'/></a>
+                        <a href='https://www.facebook.com/' target="_blank" rel="noreferrer"><img src='https://res.cloudinary.com/dv4pkbmin/image/upload/v1618498698/Icon/instagram_uud769.png' alt='Instagram' width='50' height='50'/></a>
+                        <a href='https://www.facebook.com/' target="_blank" rel="noreferrer"><img src='https://res.cloudinary.com/dv4pkbmin/image/upload/v1618498641/Icon/youtube_izfmni.png' alt='Youtube' width='50' height='50'/></a>
+                    </div>
                 </div>
                 <div className="col border-right">
                     <h5 className="text-light">Especialidades</h5>
@@ -53,14 +56,14 @@ import './CustomFooter.css'
                     </div>
                 </div>
             </div>
-{/*             <div className="d-flex align-items-center row justify-content-between ">
+            <div className="d-flex align-items-center row justify-content-between pt-4 second__footer ">
                 <div className="col">
                     <h6 className="text-light">Proyecto realizado por Alicia Parra Serrano y Diego Cerratos Bravo</h6>
                 </div>
                 <div className="col">
                     <h6 className="text-light" >Copyright © 2021 | Proyecto realizado con fines académicos.</h6>
                 </div>
-            </div> */}
+            </div>
             
         </div>
       );
