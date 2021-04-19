892cb1add87764d78e2dd5281b62ea5cb948b102
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-04-19 11:00:38 +0200

    Add faqs view with translation

--- src/components/Views/FAQ/FAQ.jsx
+++ src/components/Views/FAQ/FAQ.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './FAQ.css'
+import { useTranslation } from 'react-i18next';
+import { Accordion, Card } from 'react-bootstrap';
+
 
 const FAQ = () => {
+    const { t } = useTranslation();
+
+    const clickhandler = () => {
+        console.log("click")
+    }
+
+    const getFaq = () => {
+        let faqs = []
+        for(let i=1; i<23; i++){
+            faqs.push(
+                <Accordion className="my-1">
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey={i} className="accordion__style" onClick={clickhandler}>
+                            <h4 className="text-center title__style">{t(`faq.${i}.ask`)}</h4>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey={i} className="">
+                            <Card.Body>
+                                <p className="answer__style">{t(`faq.${i}.answer`)}</p>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
+            )
+        }
+        return faqs
+    }
+
     return (
-        <div>
-            <h1>FAQ</h1>
+        <div className="container">
+            {getFaq()}
         </div>
     );
 };
 
-export default FAQ;
+export default function App() {
+    return (
+      <Suspense fallback="loading">
+        <FAQ />
+      </Suspense>
+    );
+  }
