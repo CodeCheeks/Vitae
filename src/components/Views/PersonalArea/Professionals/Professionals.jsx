a2c3128470fe74500330b29fcdc89781510a6e44
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-04-20 11:19:39 +0200

    Add Profiles views and routes

---
+++ src/components/Views/PersonalArea/Professionals/Professionals.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Professionals.css'
+
+const Professionals = () => {
+    return (
+        <div>
+            <h1>Profesionales</h1>
+        </div>
+    );
+};
+
+export default Professionals;
