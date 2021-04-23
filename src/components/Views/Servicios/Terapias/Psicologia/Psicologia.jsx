6a22cf6b197371eeb7e7c1d33b40dcdab522a29c
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-04-23 19:05:48 +0200

    Add services views without translation

--- src/components/Views/Servicios/Terapias/Psicologia/Psicologia.jsx
+++ src/components/Views/Servicios/Terapias/Psicologia/Psicologia.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import './Psicologia.css'
 
 const Psicologia = () => {
+    const { t } = useTranslation();
     return (
-        <div>
-            <h1>Psicología</h1>
+        <div className="container">
+            <div className="row">
+                <h1>{t('psychology.title1')}</h1>
+            </div>
+            <div className="row align-items-center">
+                <div className="col">
+                    <p>{t('psychology.paragraph1')}</p>
+                    <p>{t('psychology.paragraph2')}</p>
+                    <p>{t('psychology.paragraph3')}</p>
+                </div>
+                <div className="col">
+                    <img src="https://res.cloudinary.com/dv7hswrot/image/upload/v1619193404/Vitae/imagenes/Servicios/Psicolog%C3%ADa/psicologo-tercera-edad_k3gvds.jpg" className="rounded" alt="psychology"/>
+                </div>
+
+            </div>
+            <div className="row">
+                <h1>{t('psychology.title2')}</h1>
+                <p>{t('psychology.paragraph4')}</p>
+            </div>
+            <div className="row">
+                <div className="col">
+                        <ul>
+                            <li>{t('psychology.types.depression')}</li>
+                            <li>{t('psychology.types.anxiety')}</li>
+                            <li>{t('psychology.types.grief')}</li>
+                            <li>{t('psychology.types.disorder')}</li>
+                            <li>{t('psychology.types.cognitive')}</li>
+                            <li>{t('psychology.types.support')}</li>
+                        </ul>
+                </div>
+            </div>
+            <div className="row">
+                <h1>{t('psychology.title3')}</h1>
+                <p>{t('psychology.paragraph5')}</p>
+                <p>{t('psychology.paragraph6')}</p>
+            </div>
         </div>
     );
 };
 
-export default Psicologia;
+
+export default function App() {
+    return (
+      <Suspense fallback="loading">
+        <Psicologia />
+      </Suspense>
+    );
+  }
--- src/components/Views/Servicios/Terapias/TerapiaOcupacional/TerapiaOcupacional.jsx
+++ src/components/Views/Servicios/Terapias/TerapiaOcupacional/TerapiaOcupacional.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
 import './TerapiaOcupacional.css'
 
 const TerapiaOcupacional = () => {
+    const { t } = useTranslation();
     return (
-        <div>
-            <h1>Terapia ocupacional</h1>
+        <div className="container">
+            <div className="row">
+                <h1>{t('therapy.title1')}</h1>
+                <p>{t('therapy.paragraph1')}</p>
+                <p>{t('therapy.paragraph1')}</p>
+                <p>{t('therapy.paragraph1')}</p>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <NavLink className='link__style logo__wrapper'to="#">
+                        <img src="https://res.cloudinary.com/dv7hswrot/image/upload/v1619197350/Vitae/imagenes/Servicios/Terapia/actividad-vida-diaria_lyrwez.jpg" className="m-4 aid__img" alt={t('therapy.types.avd.title')}/>
+                        <h4 className="custom__style">{t('therapy.types.avd.title')}</h4>
+                    </NavLink>
+                    <NavLink className='link__style logo__wrapper'to="#">
+                        <img src="https://res.cloudinary.com/dv7hswrot/image/upload/v1619197146/Vitae/imagenes/Servicios/Terapia/estimulacion-sensorial_k50xvp.jpg" className="m-4 aid__img" alt={t('therapy.types.sensorial.title')}/>
+                        <h4 className="custom__style">{t('therapy.types.sensorial.title')}</h4>
+                    </NavLink>
+                </div>
+                <div className="col">
+                    <NavLink className='link__style logo__wrapper'to="#">
+                        <img src="https://res.cloudinary.com/dv7hswrot/image/upload/v1619197350/Vitae/imagenes/Servicios/Terapia/psicomotricidad_fhu4qa.jpg" className="m-4 aid__img" alt={t('therapy.types.psyco.title')}/>
+                        <h4 className="custom__style">{t('therapy.types.psyco.title')}</h4>
+                    </NavLink>
+                    <NavLink className='link__style logo__wrapper'to="#">
+                        <img src="https://res.cloudinary.com/dv7hswrot/image/upload/v1619197146/Vitae/imagenes/Servicios/Terapia/laborterapia_vc1lrg.jpg" className="m-4 aid__img" alt={t('therapy.types.labor.title')}/>
+                        <h4 className="custom__style">{t('therapy.types.labor.title')}</h4>
+                    </NavLink>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default TerapiaOcupacional;
+
+export default function App() {
+    return (
+      <Suspense fallback="loading">
+        <TerapiaOcupacional />
+      </Suspense>
+    );
+  }
