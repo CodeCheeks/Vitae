ae567989e448f82dc4eb3e7d5a5880d3b63d6798
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-04-25 13:44:00 +0200

    Working on login back errors

--- src/components/Views/Login/Login.jsx
+++ src/components/Views/Login/Login.jsx
@@ -1,172 +1,75 @@
-import './Login.css'
-import React, { Suspense, useState } from 'react';
-import { useHistory } from 'react-router';
+
+import React, { useState, Suspense } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router';
 import { useUser } from '../../../hooks/useUser';
 import { login } from '../../../services/AuthService';
 import { setAccessToken } from '../../../store/AccessTokenStore';
 
 
-// eslint-disable-next-line no-useless-escape
-const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const Employment = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { t } = useTranslation();
+    const { push } = useHistory();
+    const { getUser: doLogin } = useUser();
 
 
-const validators = {
-  email: (value) => {
-    let message;
-
-    if (!value) {
-      message = "Email is required";
-    } else if (!EMAIL_PATTERN.test(value)) {
-      message = "Email is invalid";
+    const onSubmit = (data) => {
+        login(data)
+        .then((response) => {
+          setAccessToken(response.access_token);
+          doLogin().then(() => push("/area-personal"));
+        })
+        .catch(error => {
+          console.log("-------- Auth ERROR --------")
+          //TODO GENERATE ERROR
+        })
     }
+    
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-4">
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control className={errors.email && "is-invalid"} type="email" placeholder={t('employ.email.placeholder')} {...register("email", { required: true })}/>
+                            {errors.email && <div className="invalid-feedback">{t('employ.email.error')}</div>}
+                        </Form.Group>
 
-    return message;
-  },
-  password: (value) => {
-    let message;
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control className={errors.password && "is-invalid"} type="password" placeholder="Password" 
+                            {...register("password", { 
+                                required: true,
+                                validate: {
+                                    checkPass: () => true
+                                }
+                            })}/>
+                            {errors.password && <div className="invalid-feedback">Introduzca la contraseña</div>}
+                            {errors.password && errors.password.type ==="checkPass" && <div className="invalid-feedback">Contraseña o correo incorrectos</div>}
+                        </Form.Group>
 
-    if (!value) {
-      message = "Password is required";
-    } else if (value && value.length < 8) {
-      message = "Incorrect password";
-    }
-
-    return message;
-  },
-};
-
-const Login = () => {
-  const { push } = useHistory();
-  const { getUser: doLogin } = useUser();
-  const { t } = useTranslation();
-
-  const [state, setState] = useState({
-    fields: {
-      email: "",
-      password: "",
-    },
-    errors: {
-      email: validators.email(),
-      password: validators.password(),
-    },
-  });
-
-  const [touched, setTouched] = useState({});
-
-  const isValid = () => {
-    const { errors } = state;
-    return !Object.keys(errors).some((error) => errors[error]);
-  };
-
-  const onSubmit = (e) => {
-    const { fields } = state;
-    e.preventDefault();
-
-    if (isValid()) {
-      login(fields).then((response) => {
-        setAccessToken(response.access_token);
-        doLogin().then(() => push("/area-personal"));
-      });
-    }
-  };
-
-  const onChange = (e) => {
-    const { name, value } = e.target;
-
-    setState((prevState) => ({
-      fields: {
-        ...prevState.fields,
-        [name]: value,
-      },
-      errors: {
-        ...prevState.errors,
-        [name]: validators[name] && validators[name](value),
-      },
-    }));
-  };
-
-  const onBlur = (e) => {
-    const { name } = e.target;
-
-    setTouched((prevTouched) => ({
-      ...prevTouched,
-      [name]: true,
-    }));
-  };
-
-  const onFocus = (e) => {
-    const { name } = e.target;
-
-    setTouched((prevTouched) => ({
-      ...prevTouched,
-      [name]: false,
-    }));
-  };
-
-  const { email, password } = state.fields;
-  const { errors } = state;
-
- 
-  return (
-
-    <div className="Login mt-4 container d-flex justify-content-center">
-      <form onSubmit={onSubmit} style={{ maxWidth: 500 }}>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-          {t('login.email.title')}
-          </label>
-          <input
-            className={`form-control custom__input ${touched.email && errors.email ? "is-invalid" : ""}`}
-            type="email"
-            id="email"
-            name="email"
-            autoComplete="off"
-            value={email}
-            onChange={onChange}
-            onBlur={onBlur}
-            onFocus={onFocus}
-          />
-          <div className="invalid-feedback">{errors.email==="Email is required" ?  t('login.email.error.required') : t('login.email.error.invalid')}</div>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
+            </div>
         </div>
-
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-          {t('login.pass.title')}
-          </label>
-          <input
-            className={`form-control ${
-              touched.password && errors.password ? "is-invalid" : ""
-            }`}
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={onChange}
-            onBlur={onBlur}
-            onFocus={onFocus}
-          />
-          <div className="invalid-feedback">{errors.password==="Password is required" ?  t('login.pass.error.required') : t('login.pass.error.invalid')}</div>
-        </div>
-
-        <button
-          type="submit"
-          className="btn btn-outline-primary"
-          disabled={!isValid()}
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+       
+    );
 };
 
 
 
 export default function App() {
-  return (
-    <Suspense fallback="loading">
-      <Login />
-    </Suspense>
-  );
+    return (
+        <Suspense fallback="loading">
+        <Employment />
+      </Suspense>
+    );
 }
